d2ea4d1b007fa43eb085757173601d0d1b24d49e
brezginaor <obrezgina@mail.ru>
2025-12-15 16:55:20 +0300

    add single

--- metropolis-mpi/run_c_mpi_once_tornado.sh
+++ metropolis-mpi/run_c_mpi_once_tornado.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=metro_c_omp_once
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=48
-#SBATCH --time=00:20:00
+#SBATCH --job-name=metro_c_mpi_once
+#SBATCH --nodes=4
+#SBATCH --ntasks=112
+#SBATCH --cpus-per-task=1
+#SBATCH --time=01:30:00
 #SBATCH --partition=tornado
-#SBATCH --output=metro_c_omp_once_%j.out
-#SBATCH --error=metro_c_omp_once_%j.err
+#SBATCH --output=%x_%j.out
 
 set -euo pipefail
 
-module purge
-module load compiler/gcc/11
-
 ROOT="$HOME/metropolis-mpi"
-cd "$ROOT/metropolis-omp"
+LOGDIR="$ROOT/logs"
+mkdir -p "$LOGDIR"
 
-# без warning про rand_r
-gcc -O3 -std=c99 -D_POSIX_C_SOURCE=200112L -fopenmp -DVERBOSE \
-  metropolis_omp.c -o metropolis_omp -lm
-
-TOTAL_T=10000000
-SIGMA=0.5
-OMP_THREADS="${SLURM_CPUS_PER_TASK}"
+# чтобы лог лежал в logs/
+#exec >"$LOGDIR/metro_c_mpi_once_${SLURM_JOB_ID}.out" 2>&1
 
 echo "Date              = $(date)"
 echo "Hostname          = $(hostname -s)"
@@ -32,11 +24,28 @@ echo "Nodes Allocated   = ${SLURM_JOB_NUM_NODES}"
 echo "Tasks Allocated   = ${SLURM_NTASKS}"
 echo "Cores/Task        = ${SLURM_CPUS_PER_TASK}"
 echo ""
-echo "Run config: OMP_NUM_THREADS=${OMP_THREADS} TOTAL_T=${TOTAL_T} SIGMA=${SIGMA}"
+
+module load compiler/gcc/11 mpi/openmpi/4.1.6/gcc/11
+
+cd "$ROOT/metropolis-mpi"
+
+INC="-I$HOME/sprng_lib/sprng/include"
+LIB="-L$HOME/sprng_lib/sprng/lib -llcg -lm"
+
+BIN="metropolic_${SLURM_JOB_ID}"
+mpicc -O3 -std=c99 metropolic.c -o "$BIN" $INC $LIB
+
+# параметры (можно поменять)
+P="${1:-112}"          # сколько процессов запустить
+TOTAL_T="${2:-100000000}"
+SIGMA="${3:-0.5}"
+QUIET=0                # 0 => полный вывод
+
+echo "Run config: P=$P TOTAL_T=$TOTAL_T SIGMA=$SIGMA QUIET=$QUIET"
 echo ""
 
-export OMP_NUM_THREADS="$OMP_THREADS"
-export OMP_PROC_BIND=close
-export OMP_PLACES=cores
+mpirun -np "$P" --mca btl ^openib "./$BIN" "$TOTAL_T" "$SIGMA" "$QUIET"
 
-srun -n 1 -c "${SLURM_CPUS_PER_TASK}" ./metropolis_omp "$TOTAL_T" "$SIGMA"
+echo ""
+echo "Done."
+echo "Log: $LOGDIR/metro_c_mpi_once_${SLURM_JOB_ID}.out"
